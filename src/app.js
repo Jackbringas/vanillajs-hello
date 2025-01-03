fc07974199d7a615f383d163c4371004c30fee6b
Jackbringas <102544709+Jackbringas@users.noreply.github.com>
2025-01-03 22:37:01 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,42 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["the dog", "my mom", "my husband", "Aaron", "Jack", "my husband"];
+  let action = ["ate", "hid", "cooked", "stole", "slept", "broke", "crushed"];
+  let what = [
+    "my homework",
+    "my phone",
+    "my book",
+    "my house",
+    "the dinner",
+    "the car"
+  ];
+  let when = [
+    "before the class",
+    "at morning",
+    "yesterday",
+    "when I was eating",
+    "when I was sleeping",
+    "when I was at GYM"
+  ];
+
+  function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  function getRandom(anyArray) {
+    let max = who.length - 1;
+    let min = 0;
+    let random = getRandomNumber(min, max);
+    return anyArray[random];
+  }
+  document.querySelector("#excuse").innerHTML =
+    getRandom(who) +
+    " " +
+    getRandom(action) +
+    " " +
+    getRandom(what) +
+    " " +
+    getRandom(when);
 };
